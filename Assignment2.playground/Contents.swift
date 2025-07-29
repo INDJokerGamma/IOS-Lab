877ac866a4c94403e6f2f7b5f629b7c603c1280c
Piyush <piyushdawkhare0000@gmail.com>
2025-07-29 11:20:00 +0530

    Initializing the repo

---
+++ Assignment2.playground/Contents.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+//Q1
+var a = 25;
+var b = 5;
+
+print("Addition of a and b is: ", a+b);
+print("Substartion of a and b is: ", a-b);
+print("Multiplication of a and b is: ", a*b);
+print("Divison of a and b is: ", a/b);
+
+
+//Q2
+print("  ");
+print("Inital value of b is: ", b);
+b += 5;
+print("After += 5:",b);
+b -= 5;
+print("After -= 5:", b);
+b *= 5;
+print("After *= 5:", b);
+b /= 5;
+print("After /= 5:", b);
+
+
+//Q3
+print(" ")
+
